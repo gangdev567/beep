3edf7f8363e5426514bd5fcdec229a8530bf4816
dlvlfdn567 <dlvlfdn567@gmail.com>
2024-01-24 17:09:13 +0900

    초기 설정 관련 업데이트

---
+++ src/main/java/com/itwill/beep/BeepApplication.java
@@ -0,0 +1,13 @@
+package com.itwill.beep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeepApplication.class, args);
+	}
+
+}
