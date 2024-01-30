eec62f3db61c06e3e178f8c32d1eaedf020c27a8
HW Kim <skey7463@naver.com>
2024-01-30 12:11:00 +0900

    수정

---
+++ src/main/java/com/itwill/beep/web/HomeController.java
@@ -0,0 +1,19 @@
+package com.itwill.beep.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		log.info("home()");
+		
+		return "home";
+	}
+
+}
