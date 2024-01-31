1da585bddd1b33d045598af5ca4326a5eda29414
admin <minkyu45@naver.com>
2024-01-31 15:28:56 +0900

    category fix, follow code test complete

--- src/main/java/com/itwill/beep/web/CategoryController.java
+++ src/main/java/com/itwill/beep/web/CategoryController.java
@@ -2,14 +2,18 @@ package com.itwill.beep.web;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.itwill.beep.domain.Account;
 import com.itwill.beep.domain.Category;
 import com.itwill.beep.service.CategoryService;
+import com.itwill.beep.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,13 +24,18 @@ import lombok.extern.slf4j.Slf4j;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final UserService userService;
 
     @GetMapping
     public String showCategories(Model model) {
         log.info("showCategories()");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Account user = userService.loginUser(authentication.getName());
+
         model.addAttribute("categories", categoryService.findAllAndSaveCategories());
         model.addAttribute("mostViewers", categoryService.findByTotalViewers());
-        log.info("size={}", categoryService.findByTotalViewers().size());
+        model.addAttribute("user", user);
 
         return "categories";
     }
