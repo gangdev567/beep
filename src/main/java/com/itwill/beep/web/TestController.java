1dbec5577fdcbf557027c2574619ac1bbdc8644e
admin <minkyu45@naver.com>
2024-02-05 17:23:57 +0900

    follow 목록, follow 등록/취소 명칭 수정 테스트 html 삭제

--- src/main/java/com/itwill/beep/web/TestController.java
+++ src/main/java/com/itwill/beep/web/TestController.java
@@ -1,18 +1,9 @@
 package com.itwill.beep.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@RequiredArgsConstructor
+
 @Controller
 public class TestController {
-
-    @GetMapping("/followtestpage")
-    public void test() {
-
-    }
 
 }
