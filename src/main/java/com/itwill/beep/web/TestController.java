4e82ddfe51436519f6ea59534fe5dcd904fad260
admin <minkyu45@naver.com>
2024-02-06 09:50:06 +0900

    save

--- src/main/java/com/itwill/beep/web/TestController.java
+++ src/main/java/com/itwill/beep/web/TestController.java
@@ -1,9 +1,6 @@
 package com.itwill.beep.web;
 
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -11,15 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class TestController {
-	
-	
-	
-    @GetMapping("/followtestpage")
-    public void test() {
 
-    }
-
-
-
+}
 
 
