7bb21626507b0f76c57121848d3aa92e7e32f0ac
dlvlfdn567 <dlvlfdn567@gmail.com>
2024-02-14 11:20:00 +0900

    방송 구현 오류 수정

--- src/main/java/com/itwill/beep/web/StreamingController.java
+++ src/main/java/com/itwill/beep/web/StreamingController.java
@@ -71,7 +71,8 @@ public class StreamingController {
 
             // 스트리머의 스트림키를 기반으로 스트리밍 URL 생성
             String streamingKey = user.getUserStreamingKey(); // 스트리머의 스트리밍 키를 가져온다.
-            String streamingUrl = String.format("http://localhost:8088/stream/hls/%s.m3u8", streamingKey); // 스트리밍 URL 동적 생성
+            log.info("streamingKey = {}", streamingKey);
+            String streamingUrl = String.format("http://localhost:8088/streaming/hls/%s.m3u8", streamingKey); // 스트리밍 URL 동적 생성
             model.addAttribute("streamingUrl", streamingUrl);
 
             // TODO: 브로드캐스트 상태를 온으로 만들고 팔로워에게 알림을 보내도록
