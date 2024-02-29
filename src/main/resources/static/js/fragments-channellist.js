a0fac24fe1062566b7b3b2c38e5e08b60be7d816
HW Kim <skey7463@naver.com>
2024-02-29 17:20:31 +0900

    알림 pull

--- src/main/resources/static/js/fragments-channellist.js
+++ src/main/resources/static/js/fragments-channellist.js
@@ -125,7 +125,7 @@ async function fetchFollowList() {
                     divTag.style.border = "none";
 
                     divTag.addEventListener('mouseenter', () => {
-                        divTag.style.backgroundColor = '#808080'; // 회색 빛 추가
+                        divTag.style.backgroundColor = '#695CFE'; // 회색 빛 추가
                     });
 
                     divTag.addEventListener('mouseleave', () => {
@@ -146,6 +146,11 @@ async function fetchFollowList() {
                     img.alt = '프로필 이미지';
                     img.style.width = '50px';
                     img.style.height = '50px';
+                    img.style.borderRadius = '50%'; // 보더 레디어스 추가
+
+					// 호버 효과를 위한 스타일
+					img.style.transition = 'border 0.3s'; // 트랜지션 효과 추가
+					img.style.border = '1px solid transparent'; // 초기에 투명한 테두리 설정
                     img.classList.add('me-3');
                     div.appendChild(img); // 이미지를 <div> 태그 내에 추가
 
@@ -285,7 +290,7 @@ async function fetchPopularChannels() {
                     divTag.style.border = "none";
 
                     divTag.addEventListener('mouseenter', () => {
-                        divTag.style.backgroundColor = '#808080'; // 회색 빛 추가
+                        divTag.style.backgroundColor = '#695CFE'; // 보라 빛 추가
                     });
 
                     divTag.addEventListener('mouseleave', () => {
@@ -306,6 +311,11 @@ async function fetchPopularChannels() {
                     img.alt = '프로필 이미지';
                     img.style.width = '50px';
                     img.style.height = '50px';
+                    img.style.borderRadius = '50%'; // 보더 레디어스 추가
+
+					// 호버 효과를 위한 스타일
+					img.style.transition = 'border 0.3s'; // 트랜지션 효과 추가
+					img.style.border = '1px solid transparent'; // 초기에 투명한 테두리 설정
                     img.classList.add('me-3');
                     div.appendChild(img); // 이미지를 <div> 태그 내에 추가
 
